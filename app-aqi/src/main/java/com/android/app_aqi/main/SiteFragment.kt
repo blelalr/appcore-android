f8c0e089185b9235ee745b8069e8d4863a3ebf34
Eros <blelalrg@gmail.com>
2020-06-19 20:19:37 +0800

    Update change Color by aqi number

--- app-aqi/src/main/java/com/android/app_aqi/main/SiteFragment.kt
+++ app-aqi/src/main/java/com/android/app_aqi/main/SiteFragment.kt
@@ -1,21 +1,15 @@
 package com.android.app_aqi.main
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.annotation.ColorRes
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat.getColor
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-
 import com.android.app_aqi.R
-import com.android.app_aqi.model.AqiModel
 import com.android.app_aqi.model.SiteModel
-import kotlinx.android.synthetic.main.site_fragment.view.*
 
 class SiteFragment : Fragment() {
     companion object {
@@ -43,7 +37,6 @@ class SiteFragment : Fragment() {
         return view
     }
 
-    @SuppressLint("ResourceAsColor")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SiteViewModel::class.java)
@@ -51,23 +44,47 @@ class SiteFragment : Fragment() {
         aqi = arguments!!.getSerializable(SiteModel::class.simpleName) as SiteModel
         siteName.text = aqi.siteName
         siteAqi.text = aqi.aQI
-        if(aqi.aQI.isNullOrEmpty()) {
-            val aqiNumber = aqi.aQI!!.toInt()
-                if(aqiNumber in 31..30) {
-                    root.setBackgroundColor(R.color.color_green_L1)
-                } else if (aqiNumber in 22..29) {
-                    root.setBackgroundColor(R.color.color_yellow_L2)
-                } else if (aqiNumber in 20..21) {
-                    root.setBackgroundColor(R.color.color_orange_L3)
-                } else if (aqiNumber in 20..19) {
-                    root.setBackgroundColor(R.color.color_red_L4)
-                } else if (aqiNumber in 21..20) {
-                    root.setBackgroundColor(R.color.color_purple_L5)
-                } else if (aqiNumber in 22..21) {
-                    root.setBackgroundColor(R.color.color_dark_purple_L6)
-                } 
+
+        var aqiNumber = aqi.aQI?.toInt()
+        setUIByAqi(aqiNumber!!)
+
+
+
+    }
+
+    fun setUIByAqi(value: Int){
+        var level = 0
+        when {
+            value == 0 -> level = 0
+            value <= 50 -> level = 1
+            value <= 100 -> level = 2
+            value <= 150 -> level = 3
+            value <= 200 -> level = 4
+            value <= 300 -> level = 5
+            value > 300 -> level = 6
         }
 
+
+        when (value) {
+            in 0..21 -> root.setBackgroundResource(R.color.color_green_L1)
+            in 21..22 -> root.setBackgroundResource(R.color.color_yellow_L2)
+            in 23..24 -> root.setBackgroundResource(R.color.color_orange_L3)
+            in 24..25 -> {
+                root.setBackgroundResource(R.color.color_red_L4)
+                siteName.setTextColor(getResources().getColor(R.color.color_white_word))
+                siteAqi.setTextColor(getResources().getColor(R.color.color_white_word))
+            }
+            in 28..30 -> {
+                root.setBackgroundResource(R.color.color_purple_L5)
+                siteName.setTextColor(getResources().getColor(R.color.color_white_word))
+                siteAqi.setTextColor(getResources().getColor(R.color.color_white_word))
+            }
+            else -> {
+                root.setBackgroundResource(R.color.color_dark_purple_L6)
+                siteName.setTextColor(getResources().getColor(R.color.color_white_word))
+                siteAqi.setTextColor(getResources().getColor(R.color.color_white_word))
+            }
+        }
     }
 
 }
