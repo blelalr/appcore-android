a36f00f9ffe2708b017ec93d62fb9654de4c2a56
CI-Esther.Chen <>
2020-10-21 17:26:42 +0800

    remove useless code.

--- app-aqi/src/main/java/com/android/app_aqi/UpdateAQIWorker.kt
+++ app-aqi/src/main/java/com/android/app_aqi/UpdateAQIWorker.kt
@@ -1,19 +1,9 @@
 package com.android.app_aqi
 
 import android.content.Context
-import android.util.Log
-import androidx.room.Room
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.android.app_aqi.api.TaskManager
-import com.android.app_aqi.model.AqiModel
-import com.android.app_aqi.model.RequestEntity
 import com.android.app_aqi.repository.AqiRepositoryImpl
-import com.android.app_aqi.room.AqiDatabase
-import com.android.app_aqi.task.AqiDataTask
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import okhttp3.ResponseBody
 
 class UpdateAQIWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
 
